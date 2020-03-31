5d1a188ccf0d608023674e4c742c2e349ff838ca
keaichong <844746224@qq.com>
2020-03-31 23:48:03 +0800

    自定义菜单完成 修改promise

--- wechat/menu.js
+++ wechat/menu.js
@@ -3,55 +3,45 @@
  */
 module.exports = {
     "button": [{
-            "type": "click",
-            "name": "今日歌曲",
-            "key": "V1001_TODAY_MUSIC"
+        "type": "click",
+        "name": "今日歌曲",
+        "key": "V1001_TODAY_MUSIC"
+    },
+    {
+        "name": "菜单",
+        "sub_button": [{
+            "type": "view",
+            "name": "去百度",
+            "url": "http://www.baidu.com/"
         },
         {
-            "name": "菜单",
-            "sub_button": [{
-                    "type": "view",
-                    "name": "去百度",
-                    "url": "http://www.baidu.com/"
-                },
-                {
-                    "type": "click",
-                    "name": "赞一下我们",
-                    "key": "V1001_GOOD"
-                },
-                // {
-                //     "type": "media_id", 
-                //     "name": "图片", 
-                //     "media_id": "MEDIA_ID1"
-                //  }, 
-                //  {
-                //     "type": "view_limited", 
-                //     "name": "图文消息", 
-                //     "media_id": "MEDIA_ID2"
-                //  }
-                {
-                    "name": "发图", 
-                    "sub_button": [
-                        {
-                            "type": "pic_sysphoto", 
-                            "name": "系统拍照发图", 
-                            "key": "系统拍照发图1", 
-                         }, 
-                        {
-                            "type": "pic_photo_or_album", 
-                            "name": "拍照或者相册发图", 
-                            "key": "拍照或者相册发图1", 
-                        }, 
-                        {
-                            "type": "pic_weixin", 
-                            "name": "微信相册发图", 
-                            "key": "微信相册发图1", 
-                        }
-                    ]
-                }, 
-            ]
-        }
-        
+            "type": "click",
+            "name": "赞一下我们",
+            "key": "V1001_GOOD"
+        },
+        ]
+    },
+    {
+        "name": "发图",
+        "sub_button": [
+            {
+                "type": "pic_sysphoto",
+                "name": "系统拍照发图",
+                "key": "系统拍照发图1",
+            },
+            {
+                "type": "pic_photo_or_album",
+                "name": "拍照或者相册发图",
+                "key": "拍照或者相册发图1",
+            },
+            {
+                "type": "pic_weixin",
+                "name": "微信相册发图",
+                "key": "微信相册发图1",
+            }
+        ]
+    },
+
     ]
 
 }
--- wechat/wechat.js
+++ wechat/wechat.js
@@ -48,12 +48,10 @@ class Wechat {
     saveAccessToken(AccessToken) {
         return new Promise((resolve, reject) => {
             writeFile("./AccessToken.txt", JSON.stringify(AccessToken), err => {
-                if (!err)
-                {
+                if (!err) {
                     console.log("AccessToken保存成功");
                     resolve();
-                } else
-                {
+                } else {
                     reject(err);
                 }
             });
@@ -62,13 +60,11 @@ class Wechat {
     readAccessToken(AccessToken) {
         return new Promise((resolve, reject) => {
             readFile("./AccessToken.txt", (err, data) => {
-                if (!err)
-                {
+                if (!err) {
                     console.log("AccessToken读取成功", data);
                     data = JSON.parse(data);
                     resolve(data);
-                } else
-                {
+                } else {
                     reject("读取失败");
                 }
             });
@@ -76,8 +72,7 @@ class Wechat {
     }
     isVaildAccessToken(data) {
         // 检测传入参数是否有效
-        if (!data && !data.access_token && !data.expires_in)
-        {
+        if (!data && !data.access_token && !data.expires_in) {
             // access_token无效
             return false;
         }
@@ -86,50 +81,46 @@ class Wechat {
     }
     fetchAccessToken() {
         // 优化 内存里面有access——token就不用去文件中读取
-        if (this.access_token && this.expires_in && this.isVaildAccessToken(this))
-        {
-            console.log("缓存");
+        if (this.access_token && this.expires_in && this.isVaildAccessToken(this)) {
+            console.log("有缓存");
             return Promise.resolve({
-                access_token: res.access_token,
-                expires_in: res.expires_in
+                access_token: this.access_token,
+                expires_in: this.expires_in
             });
         }
-        return new Promise((resolve, reject) => {
-            //，只要一个 await 后面的Promise状态变为 rejected ，整个 async 函数都会中断执行 进入到try catch里面的catch里面
-            this.readAccessToken().then(res => {
-                if (this.isVaildAccessToken(res))
-                {
-                    // 没有过期
-                    resolve(res);
-                } else
-                {
-                    const res = this.getAccessToken();
-                    this.saveAccessToken(res);
-                    // 不用 resolve(res) 这样可以返回一个promise对象然后在catch后面继续点then 把res数据做个对象缓存
-                    return Promise.resolve(res);
-                }
-            }).catch(async err => {
-                console.log("失败" + err);
+
+        //return 一个promise对象
+        return this.readAccessToken().then(async res => {
+            if (this.isVaildAccessToken(res)) {
+                console.log('没有过期')
+                // 没有过期（return一个promise对象 会进入下一个点then）
+                return Promise.resolve(res);
+            } else {
+                console.log('已经过期了')
                 const res = await this.getAccessToken();
                 this.saveAccessToken(res);
+                // 不用 resolve(res) 这样可以返回一个promise对象然后在catch后面继续点then 把res数据做个对象缓存
                 return Promise.resolve(res);
-            })
-                .then(res => {
-                    //在内存里存下 不然每次都需要去读取文件
-                    console.log("设置缓存");
-                    this.access_token = res.access_token;
-                    this.expires_in = res.expires_in;
-                    return Promise.resolve(res);
-                });;
-        })
+            }
+        }).catch(async err => {
+            console.log("失败" + err);
+            const res = await this.getAccessToken();
+            this.saveAccessToken(res);
+            return Promise.resolve(res);
+        }).then(res => {
+            //在内存里存下 不然每次都需要去读取文件     
+            this.access_token = res.access_token;
+            this.expires_in = res.expires_in;
+            // return一个promise对象  在通过return 返回给调用
+            return Promise.resolve(res);
+        });;
 
     }
     // 创建自定义菜单
     creatMenu(menu) {
         //获取菜单
         return new Promise(async (resolve, reject) => {
-            try
-            {
+            try {
                 //async await 不会抛出异常 所以用trycatch  只有一行报错就会走catch 只要一个 await 后面的Promise状态变为 rejected ，整个 async 函数都会中断执行 进入到try catch里面的catch里面
                 const data = await this.fetchAccessToken();
                 // 地址
@@ -142,19 +133,17 @@ class Wechat {
                     body: menu
                 });
                 resolve(result);
-            } catch (e)
-            {
+            } catch (e) {
                 reject("creatmenu报错" + e);
-            }
+            }                          
         });
     }
     // 删除自定义菜单
     deleteMenu() {
         return new Promise(async (resolve, reject) => {
-            try
-            {
+            try {
                 const data = await this.fetchAccessToken();
-                console.log({data})
+                console.log({ data }, '删除菜单')
                 //定义请求地址
                 const url = `https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=${data.access_token}`;
                 const result = await rp({
@@ -163,8 +152,7 @@ class Wechat {
                     json: true
                 });
                 resolve(result);
-            } catch (e)
-            {
+            } catch (e) {
                 reject("deleteMenu报错" + e);
             }
         });
@@ -176,7 +164,7 @@ class Wechat {
     const w = new Wechat();
     // 删除之前定义的菜单 才能创建新的菜单
     let result = await w.deleteMenu();
-    console.log(result);
+    console.log(result, '删除菜单成功');
     result = await w.creatMenu(menu);
-    console.log(result);
+    console.log(result, '创建菜单成功');
 })();
